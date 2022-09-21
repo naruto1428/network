4592ef8cfa3222df176006c5c9def21e6b232a23
Naruto Uzumaki <104317280+naruto1428@users.noreply.github.com>
2022-09-21 09:51:05 +0530

    Add files via upload

---
+++ paliudpserver.c
@@ -0,0 +1,42 @@
+#include<arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+void main() {
+struct sockaddr_in client_address,server_address;
+int socket_descriptor,n,flag,left,right;
+char message[10];
+socket_descriptor= socket(AF_INET,SOCK_DGRAM,0);
+server_address.sin_family = AF_INET;
+server_address.sin_port = 3000;
+server_address.sin_addr.s_addr= inet_addr("127.0.0.1");
+bind(socket_descriptor,(struct sockaddr *)&server_address,sizeof(server_address));
+n=sizeof(client_address);
+while(1)
+{recvfrom(socket_descriptor,&message,sizeof(message),0,(struct sockaddr *)&client_address,&n);
+printf("\nThe String recieved is:%s\n",message);
+int n=strlen(message);
+if(n==0)
+flag=1;
+else
+{
+left=0;
+right=n-1;
+flag=1;
+while(left<right && flag)
+{
+if(message[left]!=message[right])
+flag=0;
+else
+{
+left++;
+right--;
+}
+}
+}
+sendto(socket_descriptor,&flag,sizeof(int),0,(struct sockaddr *)&client_address,n);
+}
+}
